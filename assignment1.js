fe2d3a2a7ab785d7df7f1662099a4bd6d3a336f1
Abass Ibrahim <profbass4all@gmail.com>
2024-03-26 18:22:08 +0100

    new updates

--- assignment1.js
+++ assignment1.js
@@ -1,61 +1,64 @@
 // question1
 
-// let num = 0;
+let num = 0;
 
-// if(num > 0){
-//     console.log('Positive')
-// }else if(num < 0){
-//     console.log('Negative')
-// }else{
-//     console.log('Zero')
-// }
+if(num > 0){
+    console.log('Positive')
+}else if(num < 0){
+    console.log('Negative')
+}else{
+    console.log('Zero')
+}
 
 // question2
 
-// let num = 4;
+let num2 = 4;
 
-// if (num%2 == 0){
-//     console.log('Even')
-// }else{
-//     console.log('Odd')
-// }
+if (num2 % 2 == 0){
+    console.log('Even')
+}else{
+    console.log('Odd')
+}
 
 // question3
-// let num  = 11;
 
-// if(num % 3 == 0){
-//     console.log('Multiple of 3')
-// }else if(num % 5 == 0){
-//     console.log('Multiple of 5')
-// }else{
-//     console.log('Neither')
-// }
+let num3  = 11;
+
+if(num3 % 3 == 0){
+    console.log('Multiple of 3')
+}else if(num3 % 5 == 0){
+    console.log('Multiple of 5')
+}else{
+    console.log('Neither')
+}
 
 // question4
 
-// let year = 1200;
+let year = 1200;
 
-// if (year >= 1901 && year <= 2000){
-//     console.log('20th Century')
-// }else if(year >= 2001 && year <= 2100){
-//     console.log('21st Century')
-// }else if(year >= 2101 && year <= 2200){
-//     console.log('22nd Century')
-// }else{
-//     console.log('Others')
-// }
+if (year >= 1901 && year <= 2000){
+    console.log('20th Century')
+}else if(year >= 2001 && year <= 2100){
+    console.log('21st Century')
+}else if(year >= 2101 && year <= 2200){
+    console.log('22nd Century')
+}else{
+    console.log('Others')
+}
 
-// let value = 1000;
-// let result ;
+// question5
 
-// if(value < 10){
-//     result = 0
-// }else if(value >= 10 && value <= 50){
-//     result = 1
-// }else if( value >= 51 && value <= 100){
-//     result = 2
-// }else{
-//     result = 3
-// }
+let value = 1000;
+let result;
 
-// console.log(result)
+if(value < 10){
+    result = 0
+}else if(value >= 10 && value <= 50){
+    result = 1
+}else if( value >= 51 && value <= 100){
+    result = 2
+}else{
+    result = 3
+}
+
+console.log(result)
