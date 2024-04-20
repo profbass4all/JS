709df8d56ed23f1212802174f631b547304065ed
Abass Ibrahim <profbass4all@gmail.com>
2024-04-21 00:19:49 +0100

    deleted

--- assignmentArray.js
+++
@@ -1,7 +0,0 @@
-let array1 = ['Sherifat', 'Asisat', 'Abass', 'Majeed']
-                            // array       number
-const returnNElement = (arrayName, numberOfElements)=>{
-        let renewedArray = arrayName.slice(-numberOfElements)
-        return renewedArray
-}
-console.log(returnNElement(array1, 2))
